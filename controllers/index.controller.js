8288e76039644421250086013b41b8d5b03bf2a0
threevanny <54084164+threevanny@users.noreply.github.com>
2020-06-08 10:31:33 -0500

    route movie controllers

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,7 +2,38 @@ const indexCtrl = {};
 const Movie = require('../models/Movie');
 
 indexCtrl.renderIndex = (req, res) => {
-  res.render('index', { title: 'Express' });
-}
+  // render view index
+  const movies = Movie.find({}).sort({ title: 'asc' });
+  res.render('index', { title: 'Movies', movies });
+};
+
+indexCtrl.renderTop = (req, res) => {
+  // render view top
+  const movies = Movie.find({}).sort({ rating: 'desc' });
+  res.render('top', { title: 'Top', movies });
+};
+
+indexCtrl.renderForm = (req, res) => {
+  // render view form
+  res.render('form_movie', { title: 'Add new Movie' });
+};
+
+indexCtrl.saveMovie = async (req, res) => {
+  // save movie 
+  const newMovie = new Movie({
+    title: req.body.title,
+    synopsis: req.body.synopsis,
+    rating: req.body.rating,
+    poster: 'images/' + req.file.filename,
+    year: req.body.year
+  });
+
+  try {
+    await newMovie.save();
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect('/');
+};
 
 module.exports = indexCtrl;
