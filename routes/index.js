44bfa802f460afdfc961c774cee108152af05253
threevanny <54084164+threevanny@users.noreply.github.com>
2020-06-08 10:34:17 -0500

    handling routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  renderIndex
+  renderIndex,
+  renderForm,
+  saveMovie,
+  renderTop
 } = require('../controllers/index.controller');
 
 /* GET home page. */
 router.get('/', renderIndex);
+router.get('/new', renderForm);
+router.post('/new', saveMovie);
+router.get('/top', renderTop);
 
 module.exports = router;
